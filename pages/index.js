4427b01385f50170caa54930d5cb025cba57de17
kiran <gandikotakiran99@gmail.com>
2022-02-24 15:48:56 +0530

    navigate programatically

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router'
 function Home(){
+    const router=useRouter();
+    const handleClick=()=>{
+        console.log("Placed order successfully");
+        router.push('/product')
+    }
     return(
         <div>
             <h1>Home Page</h1>
@@ -19,6 +25,12 @@ function Home(){
             <Link href="/docs">
                 <a>Docs</a>
             </Link>
+
+            <div>
+                <button onClick={handleClick} >
+                    Place Order
+                </button>
+            </div>
 
         </div>
     )
