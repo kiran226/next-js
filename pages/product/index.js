9568bfdd7a1737dd7cfe46ae742ad2bff8f752d4
kiran <gandikotakiran99@gmail.com>
2022-02-24 15:35:56 +0530

    using replace prop

--- pages/product/index.js
+++ pages/product/index.js
@@ -21,7 +21,7 @@ function ProductList({productId=100}){
             </h2>
 
             <h2>
-                <Link href="/product/3">
+                <Link href="/product/3" replace>
                     <a>Product 3</a>
                 </Link>
             </h2>
